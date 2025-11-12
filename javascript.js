986435c142faf13de624b4e7886f19e3e2502bb9
Ernest V <ernest.viol@gmail.com>
2025-11-12 11:15:16 -0800

    Create psuedocode for rock paper scissors
    Added the psuedocode to the javascript to online the steps.

--- javascript.js
+++ javascript.js
@@ -1,1 +1,10 @@
-// 
+// create function getComputerChoice that returns rock paper or scissors randomly
+// create function getHumanChoice based on the user input will return one of the choices: rock, paper, scissors
+// create variable humanScore to keep track of the human's wins
+// create variable computerScore to keep track of the computer's wins
+// create function playRound that plays one round of rock paper scissors
+//    create variable humanChoice which contains the choice of the human
+//    create variable computerChoice which contains the choice of the computer
+//    write the result of the match to the console in the form of “You lose! Paper beats Rock”.
+//    increment either humanScore or computerScore
+// create function playGame that calls playRound 5 times and declares the winner at the end.
