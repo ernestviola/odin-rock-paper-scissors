b04173e1742638a9756436a9a299c375446c147e
Ernest V <ernest.viol@gmail.com>
2025-11-15 20:17:50 -0800

    Modify javascript to work with UI
    Modified the previous javscript to work with the new UI. Addied cycling
    to the computer choice and added the onClick event to every user button.

--- javascript.js
+++ javascript.js
@@ -1,12 +1,37 @@
 // create function getComputerChoice that returns rock paper or scissors randomly
-function getComputerChoice() {
+const ROCK = './images/rock.png'
+const PAPER = './images/paper.png'
+const SCISSORS = './images/scissors.png'
+
+async function getComputerChoice() {
+  const computersChoice = document.querySelector('.computers-choice');
+
+  for (let i = 0; i < 10; i++) {
+      const randomOfThree = Math.ceil(Math.random() * 3);
+      switch (randomOfThree) {
+        case (1):
+          computersChoice.src=ROCK
+          break;
+        case (2):
+          computersChoice.src=PAPER
+          break;
+        case (3):
+          computersChoice.src=SCISSORS
+          break;
+      }
+    await new Promise(resolve => setTimeout(resolve, 25));
+  }
+
   const randomOfThree = Math.ceil(Math.random() * 3);
   switch (randomOfThree) {
     case (1):
+      computersChoice.src=ROCK
       return 'rock';
     case (2):
+      computersChoice.src=PAPER
       return 'paper';
     case (3):
+      computersChoice.src=SCISSORS
       return 'scissors'
   }
 }
@@ -29,46 +54,73 @@ function getHumanChoice() {
   return humanChoice;
 }
 
+// create function playRound that plays one round of rock paper scissors
+async function playRound(humanChoice) {
+  //    create variable humanChoice which contains the choice of the human
+  //    create variable computerChoice which contains the choice of the computer
+  humanChoice = humanChoice.toLowerCase();
+  const humanScoreSelector = document.querySelector('.players-score')
+  const computerScoreSelector = document.querySelector('.computers-score')
+  let humanScore = Number(humanScoreSelector.textContent);
+  let computerScore = Number(computerScoreSelector.textContent);
 
-// create function playGame that calls playRound 5 times and declares the winner at the end.
-function playGame() {
-  // create variable humanScore to keep track of the human's wins
-  let humanScore = 0;
-  // create variable computerScore to keep track of the computer's wins
-  let computerScore = 0;
+  if (humanScore >= 5 || computerScore >= 5) return;
 
-  // create function playRound that plays one round of rock paper scissors
-  function playRound() {
-    //    create variable humanChoice which contains the choice of the human
-    //    create variable computerChoice which contains the choice of the computer
-    const humanChoice = getHumanChoice().toLowerCase();
-    const computerChoice = getComputerChoice().toLowerCase();
+  const computerChoice = await getComputerChoice();
+  
 
-    //    compare the choices and the choices should be case-insensitive
-    if (humanChoice === computerChoice) {
-      console.log(`It's a tie!`);
-    } 
-    // human win conditions
-    else if (humanChoice == 'rock' && computerChoice == 'scissors')  {
-      console.log(`You win the round! Rock beats scissors!`)
-      humanScore++;
-    } else if (humanChoice == 'paper' && computerChoice == 'rock') {
-      console.log(`You win the round! Paper beats rock!`)
-      humanScore++;
-    } else if (humanChoice == 'scissors' && computerChoice == 'paper') {
-      console.log(`You win the round! Scissors beats paper!`)
-      humanScore++;
-    }
-    // computer win conditions
-    else if (computerChoice == 'rock' && humanChoice == 'scissors')  {
-      console.log(`Computer wins the round! Rock beats scissors!`)
-      computerScore++;
-    } else if (computerChoice == 'paper' && humanChoice == 'rock') {
-      console.log(`Computer wins the round! Paper beats rock!`)
-      computerScore++;
-    } else if (computerChoice == 'scissors' && humanChoice == 'paper') {
-      console.log(`Computer wins the round! Scissors beats paper!`)
-      computerScore++;
-    }
+
+  //    compare the choices and the choices should be case-insensitive
+  if (humanChoice === computerChoice) {
+    console.log(`It's a tie!`);
+  } 
+  // human win conditions
+  else if (humanChoice == 'rock' && computerChoice == 'scissors')  {
+    console.log(`You win the round! Rock beats scissors!`)
+    humanScore++;
+  } else if (humanChoice == 'paper' && computerChoice == 'rock') {
+    console.log(`You win the round! Paper beats rock!`)
+    humanScore++;
+  } else if (humanChoice == 'scissors' && computerChoice == 'paper') {
+    console.log(`You win the round! Scissors beats paper!`)
+    humanScore++;
   }
+  // computer win conditions
+  else if (computerChoice == 'rock' && humanChoice == 'scissors')  {
+    console.log(`Computer wins the round! Rock beats scissors!`)
+    computerScore++;
+  } else if (computerChoice == 'paper' && humanChoice == 'rock') {
+    console.log(`Computer wins the round! Paper beats rock!`)
+    computerScore++;
+  } else if (computerChoice == 'scissors' && humanChoice == 'paper') {
+    console.log(`Computer wins the round! Scissors beats paper!`)
+    computerScore++;
+  }
+
+  humanScoreSelector.textContent = humanScore;
+  computerScoreSelector.textContent = computerScore;
+  if (humanScore >= 5) {
+    console.log('Player Wins!');
+    const resultsContainer = document.querySelector('.results');
+    resultsContainer.textContent = 'You Win!';
+    resultsContainer.style.margin = '98px 0px';
+
+    document.querySelector('.computers-choice-container').remove();
+
+  } else if (computerScore >= 5) {
+    console.log('Computer Wins!')
+    const resultsContainer = document.querySelector('.results');
+    resultsContainer.textContent = 'You Lose!';
+    resultsContainer.style.margin = '98px 0px';
+
+    document.querySelector('.computers-choice-container').remove();
+  }
+
 }
+
+const players_choices = document.querySelectorAll('button');
+players_choices.forEach(btn => {
+  btn.addEventListener('click', () => {
+    playRound(btn.value)
+  })
+})
